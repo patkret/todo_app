d4b57e3b9cb42282dbc7347321d1172a0c1f00cc
Patrycja <patrycja.kretkiewicz@gmail.com>
2018-09-04 14:24:33 +0200

    Finished alert

--- src/components/finishedAlert.vue
+++ src/components/finishedAlert.vue
@@ -2,7 +2,12 @@
 <div class="alert__container">
   <div class="alert__message">
     <button type="button" class="close__button" @click="closeAlert()"> <i class="fa fa-times"></i></button>
-    <h1>YAY! You did it!</h1>
+    <div class="alert__text">
+      <h1>Yay! You did it!</h1>
+      <img class="alert__image" src="../assets/great-job.png" />
+      <h3>You have completed all tasks for today! Good job! :) </h3>
+      <h5>Add some more if you still have things to! </h5>
+    </div>
   </div>
 </div>
 </template>
